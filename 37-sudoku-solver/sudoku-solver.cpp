56513e6ed0e8bde8ec5bb06ef3c8b3d53aca1dea
Ajay Meena <ajay63679948@gmail.com>
2026-07-01 09:07:36 +0530

    Time: 370 ms (50.46%) | Memory: 8.6 MB (94.23%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isSafe(char ch,int row ,int col, vector<vector<char>> &board){
+        for(int i=0;i<9;i++){
+                if(board[row][i]==ch){
+                    return false;   
+                }
+                if(board[i][col]==ch) return false;
+
+                int boxRow = 3 * (row / 3) + i / 3;
+                int boxCol = 3 * (col / 3) + i % 3;
+                if (board[boxRow][boxCol] == ch) return false;
+            }
+            return true;
+        }
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(isSafe(ch,i,j,board)){
+                            board[i][j]=ch;
+                            if(solve(board)){
+                              return true;
+                            }
+                            board[i][j]='.';
+                        }
+                        
+                    }
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
