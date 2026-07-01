a30703c5e0d13a407e86c08f2bafadb2539f03b9
Ajay Meena <ajay63679948@gmail.com>
2026-07-01 07:21:05 +0530

    Time: 3 ms (61.96%) | Memory: 11.3 MB (26.13%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -15,54 +15,43 @@ public:
         }
         ans.push_back(temp);
     }
-    bool isSafe(int row, int col, vector<vector<int>> &board,int n){
+    bool isSafe(int row, int col, vector<vector<int>> &board,int n,vector<bool> &rowCheck,vector<bool> &diag1Check,vector<bool> &diag2Check){
         int x=row;
         int y=col;
-        while(y>=0){
-            if(board[x][y]==1){
-                return false;
-            }
-            y--;
+        if(rowCheck[x]==false && diag1Check[x+y]==false && diag2Check[x-y+n-1]==false){
+            rowCheck[x]=true;
+            diag1Check[x+y]=true;
+            diag2Check[x-y+n-1]=true;
+            return true;
         }
-        x=row;
-        y=col;
-        while(x>=0 && y>=0){
-            if(board[x][y]==1){
-                return false;
-            }
-            x--;
-            y--;
-        }
-        x=row;
-        y=col;
-        while(x<n && y>=0){
-            if(board[x][y]==1){
-                return false;
-            }
-            x++;
-            y--;
-        }
-        return true;
+        return false;
     }
-    void solve(int col,vector<vector<string>>&ans, vector<vector<int>> &board,int n){
+    void solve(int col,vector<vector<string>>&ans, vector<vector<int>> &board,int n,vector<bool> &rowCheck,vector<bool> &diag1Check,vector<bool> &diag2Check){
         if(col==n){
             addSolution(ans,board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(isSafe(row,col,board,n,rowCheck,diag1Check,diag2Check)){
                 board[row][col]=1;
-                solve(col+1,ans,board,n);
+                solve(col+1,ans,board,n,rowCheck,diag1Check,diag2Check);
                 board[row][col]=0;
+                rowCheck[row] = false;
+                diag1Check[row + col] = false;
+                diag2Check[n - 1 + row - col] = false;
             }
+            
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         if(n==1) return {{"Q"}};
         if(n==2) return {};
+        vector<bool> rowCheck(n, false);
+        vector<bool> diag1Check(2 * n, false);
+        vector<bool> diag2Check(2 * n, false);
         vector<vector<string>> ans;
         vector<vector<int>> board(n,vector<int>(n,0));
-        solve(0,ans,board,n);
+        solve(0,ans,board,n,rowCheck,diag1Check,diag2Check);
         return ans;
     }
 };
