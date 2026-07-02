8fd02c62538ea433a0632996e105944a79551ec2
Ajay Meena <ajay63679948@gmail.com>
2026-07-02 09:46:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (71.87%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int index=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>nums[index]){
+                index=i;
+            }
+        }
+        return index;
+    }
+};
