b2232255f5533f145415eeb46e29596cec84de53
Ajay Meena <ajay63679948@gmail.com>
2026-07-16 11:40:04 +0530

    Time: 74 ms (59.14%) | Memory: 155.7 MB (75.58%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -6,13 +6,9 @@ public:
         long long ans=0;
         int maxi=nums[0];
         prefixGcd[0]=maxi;
-        for(int i=0;i<n;i++){
-            if(maxi>nums[i]){
-                prefixGcd[i]=__gcd(nums[i],maxi);
-            }else{
-                maxi=nums[i];
-                prefixGcd[i]=nums[i];
-            }
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+            prefixGcd[i] = __gcd(nums[i], maxi);
         }
         sort(prefixGcd.begin(),prefixGcd.end());
         for(int i=0;i<n/2;i++){
