5bd3cc2961c835e97a800bd21917973b80bf2096
Ajay Meena <ajay63679948@gmail.com>
2026-07-17 19:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (62.43%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
+    int solve(int n, vector<int> &dp){
+        if(n==0 || n==1){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int total=0;
+        for(int i=1;i<=n;i++){
+            total+=solve(n-i,dp)*solve(i-1,dp);
+        }
+        dp[n]=total;
+        return total;
+    }
     int numTrees(int n) {
         if(n==0 || n==1){
             return 1;
         }
-        int total=0;
-        for(int i=1;i<=n;i++){
-            total+=numTrees(n-i)*numTrees(i-1);
-        }
-        return total;
+        vector<int> dp(n+1,-1);
+        dp[0]=1;
+        dp[1]=1;
+        return solve(n,dp);
     }
 };
