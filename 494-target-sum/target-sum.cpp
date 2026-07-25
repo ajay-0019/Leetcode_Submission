a3620b6efed1dbb9bf12d414fd8f8c9491d69fb7
Ajay Meena <ajay63679948@gmail.com>
2026-07-26 00:27:43 +0530

    Time: 799 ms (13.98%) | Memory: 11.6 MB (87.82%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    void solve(int idx, int currentsum,int target,int &ans,vector<int> &nums){
-        if(currentsum==target && idx==nums.size()){
-            ans++;
+    void solve(int idx, int currentsum, int target, int &ans, vector<int> &nums) {
+        if (idx == nums.size()) {
+            if (currentsum == target)
+                ans++;
             return;
         }
-        if(idx==nums.size()){
-            return;
-        }
-        solve(idx+1,currentsum+nums[idx],target,ans,nums);
-        solve(idx+1,currentsum-nums[idx],target,ans,nums);
+
+        solve(idx + 1, currentsum + nums[idx], target, ans, nums);
+        solve(idx + 1, currentsum - nums[idx], target, ans, nums);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int ans=0;
