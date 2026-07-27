791c4a8569063ab86f8f89f2822b198e104998e7
Ajay Meena <ajay63679948@gmail.com>
2026-07-27 22:20:39 +0530

    Time: 25 ms (65.82%) | Memory: 19 MB (58.08%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    int solve(int amount,vector<int>& coins, vector<int>& dp){
-        if(amount==0){
-            return 0;
-        }
-        if(amount<0){
-            return INT_MAX;
-        }
-        if(dp[amount]!=-1){
-            return dp[amount];
-        }
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int ans=solve(amount-coins[i],coins,dp);
-            if(ans!=INT_MAX){
-                mini=min(mini,1+ans);
+    int coinChange(vector<int>& nums, int amount) {
+        int n = nums.size();
+        const int INF = 1e9 + 5;
+        sort(nums.begin(), nums.end());
+        vector<int> cache(amount+1, -1);
+        auto recurse = [&](auto&& self, int k)->int{
+            if(k==0)
+                return 0;
+            if(cache[k]!=-1)
+                return cache[k];
+            int ans = INF;
+            for(int i: nums){
+                if(i>k) break;
+                ans = min(ans, 1 + self(self, k-i));
             }
-        }
-        dp[amount]=mini;
-        return mini;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-        dp[0]=0;
-        int ans=solve(amount,coins,dp);
-        if(ans==INT_MAX){
-            return -1;
-        }
-        return ans;
+            return cache[k] = ans;
+        };
+        int ans = recurse(recurse, amount);
+        return (ans>=INF?-1:ans);
     }
 };
