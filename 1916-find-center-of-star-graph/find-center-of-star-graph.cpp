9ef70b75ac56097b0f75bd34edf4b54ad7d6d4c2
Ajay Meena <ajay63679948@gmail.com>
2026-07-29 15:16:42 +0530

    Time: 113 ms (9.22%) | Memory: 137.8 MB (5.56%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,14 +1,25 @@
+#include <algorithm>
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<edges.size();i++){
-            for(int j=0;j<edges[i].size();j++){
-                mp[edges[i][j]]++;
+        int n=edges.size()+1;
+        vector<vector<int>> adj(n+1);
+        for(auto edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        int center = -1;
+        int mx = -1;
+
+        for(int i = 1; i <= n; i++) {
+            if((int)adj[i].size() > mx) {
+                mx = adj[i].size();
+                center = i;
             }
         }
-        auto maxIt = max_element(mp.begin(), mp.end(), 
-    [](const auto& a, const auto& a2) { return a.second < a2.second; });
-    return maxIt->first;
+
+        return center;
     }
 };
