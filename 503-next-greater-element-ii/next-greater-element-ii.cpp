a11a830cd1153fed49aa561dd01dae496d0f90f5
Ajay Meena <ajay63679948@gmail.com>
2026-07-31 22:47:50 +0530

    Time: 121 ms (5.06%) | Memory: 27.9 MB (97.70%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 1; k < n; k++) {
+                int j = (i + k) % n;
+                if (nums[j] > nums[i]) {
+                    ans[i] = nums[j];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
