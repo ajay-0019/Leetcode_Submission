deeee56c18a33a666efe0e154bc34a1f8e33a1b5
Ajay Meena <ajay63679948@gmail.com>
2026-08-01 15:52:47 +0530

    Time: 4 ms (30.20%) | Memory: 13.2 MB (69.46%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public: 
+    void dfs(int i, int j,vector<vector<int>>& grid){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]!=0){
+            return;
+        }
+        grid[i][j]=1;
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i,j-1,grid);
+    }
+    int closedIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        for(int j=0;j<m;j++){
+            if(grid[0][j]==0){
+                dfs(0,j,grid);
+            }
+            if(grid[n-1][j]==0){
+                dfs(n-1,j,grid);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[i][0]==0){
+                dfs(i,0,grid);
+            }
+            if(grid[i][m-1]==0){
+                 dfs(i,m-1,grid);
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    ans++;
+                    dfs(i,j,grid);
+                }
+            }
+        }
+        return ans;
+    }
+};
