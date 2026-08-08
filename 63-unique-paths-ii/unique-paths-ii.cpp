8aa7010c7fff9f7045779a6d4e442aca1f242e74
Ajay Meena <ajay63679948@gmail.com>
2026-08-08 23:39:06 +0530

    Time: 15 ms (2.35%) | Memory: 11.9 MB (55.64%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -5,28 +5,19 @@ public:
         int m=grid[0].size();
         if (grid[0][0] == 1)
             return 0;
-        vector<vector<int>> dp(n,vector<int>(m,1));
-        bool temp=false;
+        vector<vector<int>> dp(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             if(grid[i][0]==1){
-                temp=true;
+                break;
             }
-            if(temp){
-                dp[i][0]=0;
-            }else{
-                dp[i][0]=1;
-            }
+            dp[i][0]=1;
         }
-        temp=false;
-        for(int i=0;i<m;i++){
-            if(grid[0][i]==1){
-                temp=true;
+        for(int j=0;j<m;j++){
+            if(grid[0][j]==1){
+                break;
             }
-            if(temp){
-                dp[0][i]=0;
-            }else{
-                dp[0][i]=1;
-            }
+            dp[0][j]=1;
+            
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
