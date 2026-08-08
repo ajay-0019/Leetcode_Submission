9d044716e7b59bbe9d3ceb1a3eadd08ee5063294
Ajay Meena <ajay63679948@gmail.com>
2026-08-08 14:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (46.25%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s[0]=='0'){
+            return 0;
+        }
+        int n=s.size();
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int two = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+            if(s[i-1]!='0'){
+                dp[i]+=dp[i-1];
+            }
+            if (two >= 10 && two <= 26) {
+                dp[i] += dp[i - 2]; 
+            }
+        }
+        return dp[n];
+    }
+};
