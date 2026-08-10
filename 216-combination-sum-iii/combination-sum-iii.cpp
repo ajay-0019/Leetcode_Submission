b432c6c6ee0bc6ed8109178304aba80d0ffd19f2
Ajay Meena <ajay63679948@gmail.com>
2026-08-10 21:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (97.86%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -3,10 +3,6 @@ public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> temp;
-        vector<int> nums(10);
-        for(int i=1;i<=9;i++){
-            nums[i]=i;
-        }
         auto solve=[&](auto&& self, int sum,int start){
             if(sum==n && temp.size()==k){
                 ans.push_back(temp);
@@ -17,7 +13,7 @@ public:
             }
             for(int i=start;i<=9;i++){
                 temp.push_back(i);
-                self(self,sum+nums[i],i+1);
+                self(self,sum+i,i+1);
                 temp.pop_back();
             }
         };
