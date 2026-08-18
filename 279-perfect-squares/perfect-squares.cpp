7eedd8777efc9ead7e0e99bc1a24e1c6a2724163
Ajay Meena <ajay63679948@gmail.com>
2026-08-18 12:42:55 +0530

    Time: 88 ms (30.04%) | Memory: 14 MB (28.43%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,40 +1,25 @@
-int INF=1e9+1;
+const int INF=1e9;
 class Solution {
 public:
-    int solve(vector<int> &nums, vector<int>&dp, int n){
-        if(n==0){
-            return 0;
-        }
-        if(n<0){
-            return INF;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int mini=INF;
-        for(int i=0;i<nums.size();i++){
-            int ans=solve(nums,dp,n-nums[i]);
-            if(ans!=INF){
-                mini=min(mini,1+ans);
-            }
-        }
-        dp[n]=mini;
-        return mini;
-    }
     int numSquares(int n) {
-        if(n<=1){
-            return 1;
-        }
-        int root=sqrt(n);
-        if(root*root==n){
-            return 1;
-        }
-        vector<int> nums;
-        for(int i=1;i<=root;i++){
-            nums.push_back(i*i);
-        }
+        if(n==0) return 0;
         vector<int> dp(n+1,-1);
-        int ans=solve(nums,dp,n);
-        return ans;
+        auto solve=[&](auto &&self, int sum)->int{
+            if(sum==0){
+                return 0;
+            }
+            if(sum<0){
+                return INF;
+            }
+            if(dp[sum]!=-1){
+                return dp[sum];
+            }   
+            int ans=INF;
+            for(int i=1;i*i<=sum;i++){
+                ans=min(ans,1+self(self,sum-i*i));
+            }
+            return dp[sum]=ans;
+        };
+        return solve(solve,n);
     }
 };
