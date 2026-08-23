e38cf07ce5a0883b4ac354f2f26afcbb072b90b1
Ajay Meena <ajay63679948@gmail.com>
2026-08-24 00:24:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (51.74%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char,int> map1;
-        map<char,int> map2;
-        for(int i=0;i<s.size();i++){
-            map1[s[i]]++;
+        char ans=0;
+        for(char c:s){
+            ans^=c;
         }
-        for(int i=0;i<t.size();i++){
-            map2[t[i]]++;
+        for(char c:t){
+            ans^=c;
         }
-        for(auto [key,value]:map2){
-            if(map1.find(key)==map1.end()){
-                return key;
-            }else if(map1.find(key)!=map1.end() && value!=map1[key]){
-                return key;
-            }
-        }
-       return 'a'; 
+        return ans;
     }
 };
