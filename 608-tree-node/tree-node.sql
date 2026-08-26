252832beaeec253f558a35bb539ee87ce3fdaca5
Ajay Meena <ajay63679948@gmail.com>
2026-08-26 15:35:23 +0530

    Time: 473 ms (83.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 608-tree-node/tree-node.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select id,
+    case 
+       when p_id is null then 'Root'
+       when exists(
+        select 1
+        from tree t2
+        where t2.p_id=t1.id
+       ) then 'Inner'
+       else 'Leaf'
+    end as type
+from tree t1;    
