4173ad619df5c889f69b3f2c1b7d1ba8f7968f9c
Ajay Meena <ajay63679948@gmail.com>
2026-08-31 19:22:00 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (47.44%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int hIndex(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
         int n = nums.size();
         int ans = 0;
         for (int i = 0; i < n; i++) {
