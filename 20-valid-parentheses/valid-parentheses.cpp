193c316d368072c4576dd3a0798ef80a0655996d
Ajay Meena <ajay63679948@gmail.com>
2026-09-20 12:25:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.89%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+        for(int i=0;i<str.size();i++){
+            if(str[i]=='('||str[i]=='{'||str[i]=='['){
+                s.push(str[i]);
+            }else if(!s.empty() && ((str[i]==')'&&s.top()=='(') ||
+                       (str[i]==']'&&s.top()=='[') ||
+                       (str[i]=='}'&&s.top()=='{'))){
+                s.pop();
+            }else{
+                return false;
+            }
+        }
+        if(!s.empty()){
+            return false;
+        }
+        return true;
+    }
+};
