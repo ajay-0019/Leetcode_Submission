e39cb83a7cd328feebcab49368dc467d1c0d575a
Ajay Meena <ajay63679948@gmail.com>
2026-09-20 17:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (82.41%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -3,15 +3,15 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        auto solve=[&](auto &&self, int idx)->void{
+        auto solve=[&](auto &&self,vector<int> &temp,int start)->void{
             ans.push_back(temp);
-            for(int i=idx;i<nums.size();i++){
+            for(int i=start;i<nums.size();i++){
                 temp.push_back(nums[i]);
-                self(self,i+1);
+                self(self,temp,i+1);
                 temp.pop_back();
             }
         };
-        solve(solve,0);
+        solve(solve,temp,0);
         return ans;
     }
 };
